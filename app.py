cc56623613e3ccf05a8b7e53a8375fc45fb323c5
Carlos Daniel Gomes Santana <cdgomessantana@gmail.com>
2024-04-11 18:33:04 -0300

    Criado uma tela que tenha a imagem dentro da janela

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-import tkinter as tk
+from PIL import (
+    Image,
+    ImageTk
+)
+from tkinter import (
+    Tk,
+    Label,
+    PhotoImage
+)
 
 # Criar a janela principal
-janela = tk.Tk()
+janela = Tk()
 janela.title("QR Code")
 
 # Definir largura e altura da janela (em pixels)
-largura = 400
-altura = 300
+largura = 640
+altura = 480
 
 # Obter as dimensões da tela
 largura_tela = janela.winfo_screenwidth()
@@ -17,17 +25,50 @@ x = (largura_tela - largura) // 2
 y = (altura_tela - altura) // 2
 
 # Carregar o ícone como uma imagem (substitua 'icone.ico' pelo caminho do seu ícone)
-icone = tk.PhotoImage(file="icon.png")
+icone = PhotoImage(file = "icon.png")
+
+# Carregar a imagem (substitua 'imagem.png' pelo caminho da sua imagem)
+imagem = Image.open(fp = "icon.png")
+
+# Redimensionar a imagem para o tamanho desejado
+largura_desejada = 200
+altura_desejada = 150
+imagem_redimensionada = imagem.resize(
+    (largura_desejada, altura_desejada),
+    imagem
+)
+
+# Converter a imagem redimensionada para um formato suportado pelo Tkinter
+imagem_tk = ImageTk.PhotoImage(
+    imagem_redimensionada
+)
+
+# Criar um rótulo para exibir a imagem
+label_imagem = Label(
+    master = janela,
+    image = imagem_tk
+)
+label_imagem.pack()
+
+# Garantir que a imagem seja mantida em memória
+label_imagem.imagem_tk = imagem_tk
 
 # Atribuir o ícone à janela
-janela.iconphoto(True, icone)
+janela.iconphoto(
+    default = True,
+    imagem = icone
+)
 
 # Definir a posição da janela
-janela.geometry(f"{largura}x{altura}+{x}+{y}")
+janela.geometry(
+    f"{largura}x{altura}+{x}+{y}"
+)
 
 # Impedir o redimensionamento da janela
-janela.resizable(False, False)
-
+janela.resizable(
+    width = False,
+    height = False
+)
 
 # Rodar o loop principal da aplicação
 janela.mainloop()
