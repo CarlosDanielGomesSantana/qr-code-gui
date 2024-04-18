112ec55b00ca53bcbef0882cd9b06c6109575230
Carlos Daniel Gomes Santana <cdgomessantana@gmail.com>
2024-04-17 21:16:34 -0300

    Adicionado mais legibilidade ao código seguindo o pep8

--- app.py
+++ app.py
@@ -1,141 +1,112 @@
-from PIL import (
-    Image,
-    ImageTk
-)
-from tkinter import (
-    Tk,
-    Label,
-    Entry,
-    Button,
-    PhotoImage
-)
+"""
+    Autor: Carlos Daniel Gomes Santana
+    Contato: cdgomessantana@gmail.com
+    Descrição: Esse é um aplicativo desktop que
+    cria QR Code personalizados com uma licensa
+    de software livre (BSD 2-Clause License)
+"""
+
+from tkinter import Button, Entry, Label, PhotoImage, Tk
+
+from PIL import Image, ImageTk
 
 # Criar a janela principal
 janela = Tk()
 
 # Definir largura e altura da janela (em pixels)
-largura = 640
-altura = 480
+LARGURA = 640
+ALTURA = 480
 
 # Obter as dimensões da tela
-largura_tela = janela.winfo_screenwidth()
-altura_tela = janela.winfo_screenheight()
+LARGURA_DA_TELA = janela.winfo_screenwidth()
+ALTURA_DA_TELA = janela.winfo_screenheight()
 
 # Calcular as coordenadas para o centro da tela
-x = (largura_tela - largura) // 2
-y = (altura_tela - altura) // 2
+x = (LARGURA_DA_TELA - LARGURA) // 2
+y = (ALTURA_DA_TELA - ALTURA) // 2
 
-# Carregar o ícone como uma imagem (substitua 'icone.ico' pelo caminho do seu ícone)
-icone = PhotoImage(file = "icon.png")
+# Carregar a imagem
+imagem = Image.open(fp="icon.png")
 
-# Carregar a imagem (substitua 'imagem.png' pelo caminho da sua imagem)
-imagem = Image.open(fp = "icon.png")
+# Carregar o ícone como uma imagem
+icone = PhotoImage(file="icon.png")
 
 # Redimensionar a imagem para o tamanho desejado
-largura_desejada = int(largura*0.38)
-altura_desejada = int(altura*0.38)
-imagem_redimensionada = imagem.resize(
-    size = (
-        largura_desejada,
-        altura_desejada
-    )
-)
+LARGURA_DESEJADA = int(LARGURA * 0.38)
+ALTURA_DESEJADA = int(ALTURA * 0.38)
+imagem_redimensionada = imagem.resize(size=(LARGURA_DESEJADA, ALTURA_DESEJADA))
 
 # Posicionamento da Label da imagem
 # Converter a imagem redimensionada para um formato suportado pelo Tkinter
-imagem_tk = ImageTk.PhotoImage(
-    imagem_redimensionada
-)
+imagem_tk = ImageTk.PhotoImage(imagem_redimensionada)
 
 # Criar um rótulo para exibir a imagem
-coordenada_x_label_imagem = (largura - largura_desejada)//2
-coordenada_y_label_imagem = (altura - altura_desejada)//2 - 135
+COORDENADA_X_LABEL_IMAGEM = (LARGURA - LARGURA_DESEJADA) // 2
+COORDENADA_Y_LABEL_IMAGEM = (ALTURA - ALTURA_DESEJADA) // 2 - 135
 label_imagem = Label(
-    master = janela,
-    image = imagem_tk,
-    bd = 2,
-    relief = "solid"
+    master=janela, image=imagem_tk,  # type: ignore[arg-type]
+    bd=2, relief="solid"
 )
-label_imagem.place(
-    x = coordenada_x_label_imagem,
-    y = coordenada_y_label_imagem
-)
-
-# Garantir que a imagem seja mantida em memória
-label_imagem.imagem_tk = imagem_tk
+label_imagem.place(x=COORDENADA_X_LABEL_IMAGEM, y=COORDENADA_Y_LABEL_IMAGEM)
 
 # Atribuir o ícone à janela
-janela.iconphoto(
-    True, icone
-)
+janela.iconphoto(True, icone)
 
 # Posicionamento da Entry do dados do QR Code
 # Criar uma entrada de texto
-largura_entrada = 50
-coordenada_x_entrada = (largura - largura_desejada - largura_entrada)//2 
-coordenada_y_entrada = (largura - largura_desejada)//2 + 10
+LARGURA_ENTRADA = 50
+COORDENADA_X_ENTRADA = (LARGURA - LARGURA_DESEJADA - LARGURA_ENTRADA) // 2
+COORDENADA_Y_ENTRADA = (LARGURA - LARGURA_DESEJADA) // 2 + 10
 entrada = Entry(
     janela,
-    width = largura_entrada,
+    width=LARGURA_ENTRADA,
 )
-entrada.place(
-    x = coordenada_x_entrada,
-    y = coordenada_y_entrada
-)
+entrada.place(x=COORDENADA_X_ENTRADA, y=COORDENADA_Y_ENTRADA)
 
 # Posicionamento dos Button do dados do QR Code
 # Criar um botão para gerar o QR Code
-altura_botao_gerar = 1
-largura_botao_gerar = 12
+ALTURA_BOTAO_GERAR = 1
+LARGURA_BOTAO_GERAR = 12
 botao_gerar = Button(
     janela,
     text="Gerar",
-    height=altura_botao_gerar,
-    width=largura_botao_gerar,
-    command=lambda : print("Gerar")
+    height=ALTURA_BOTAO_GERAR,
+    width=LARGURA_BOTAO_GERAR,
+    command=lambda: print("Gerar"),
 )
 
-coordenada_x_botao_gerar = (largura - botao_gerar.winfo_reqwidth())//2
-coordenada_y_botao_gerar = (largura - botao_gerar.winfo_reqheight())//2
-botao_gerar.place(
-    x = coordenada_x_botao_gerar,
-    y = coordenada_y_botao_gerar
-)
+COORDENADA_X_BOTAO_GERAR = (LARGURA - botao_gerar.winfo_reqwidth()) // 2
+COORDENADA_Y_BOTAO_GERAR = (LARGURA - botao_gerar.winfo_reqheight()) // 2
+botao_gerar.place(x=COORDENADA_X_BOTAO_GERAR, y=COORDENADA_Y_BOTAO_GERAR)
 
 # Criar um botão para baixar o QR Code
-altura_botao_baixar = 1
-largura_botao_baixar = 12
+ALTURA_BOTAO_BAIXAR = 1
+LARGURA_BOTAO_BAIXAR = 12
 botao_baixar = Button(
     janela,
     text="Baixar",
-    height=altura_botao_baixar,
-    width=largura_botao_baixar,
-    command=lambda : print("Baixar")
+    height=ALTURA_BOTAO_BAIXAR,
+    width=LARGURA_BOTAO_BAIXAR,
+    command=lambda: print("Baixar"),
 )
-coordenada_x_botao_baixar = (largura - botao_baixar.winfo_reqwidth())//2
-coordenada_y_botao_baixar = (largura - botao_baixar.winfo_reqheight() + botao_gerar.winfo_reqwidth())//2
-botao_baixar.place(
-    x = coordenada_x_botao_baixar,
-    y = coordenada_y_botao_baixar
-)
+COORDENADA_X_BOTAO_BAIXAR = (LARGURA - botao_baixar.winfo_reqwidth()) // 2
+COORDENADA_Y_BOTAO_BAIXAR = (
+    LARGURA - botao_baixar.winfo_reqheight() + botao_gerar.winfo_reqwidth()
+) // 2
+botao_baixar.place(x=COORDENADA_X_BOTAO_BAIXAR, y=COORDENADA_Y_BOTAO_BAIXAR)
 
 # Configurações da janela
 # Definir a posição da janela
-janela.geometry(
-    f"{largura}x{altura}+{x}+{y}"
-)
+janela.geometry(f"{LARGURA}x{ALTURA}+{x}+{y}")
 
 # Impedir o redimensionamento da janela
-janela.resizable(
-    width = False,
-    height = False
-)
+janela.resizable(width=False, height=False)
 
 # Adicionando um título
 janela.title("QR Code")
 
 # Definindo a cor do fundo de tela
-janela.configure(bg='#E5E1DA')
+janela.configure(bg="#E5E1DA")
 
 # Rodar o loop principal da aplicação
 janela.mainloop()
